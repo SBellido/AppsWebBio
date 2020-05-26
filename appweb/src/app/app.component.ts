1355782bc7ad817d5768fa414ac6bbee645527f1
SBellido <sebastianbellidodg@gmail.com>
2020-05-26 15:04:36 -0300

    add & delete item in array items

--- appweb/src/app/app.component.ts
+++ appweb/src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'appweb';
+  items = ['nicolas', 'julian', 'perez'];
+
+  /*agrega elemento al arreglo items*/
+  addItem() {
+    this.items.push('nuevo item');
+  }
+
+/* indice de tipo number */
+deleteItem(index: number) {
+    /*ejecuta la eliminación del indice e indica la cantidad de elementos a borrar*/
+    this.items.splice(index, 1);
+  }
 }
