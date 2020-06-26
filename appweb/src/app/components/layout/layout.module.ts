7966bc39e23cb7a1d6ae72c4e7daeda0ac0fa003
SBellido <sebastianbellidodg@gmail.com>
2020-06-26 11:31:29 -0300

    little changes, nav logo and buttons. Components need to be modularized

---
+++ appweb/src/app/components/layout/layout.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+
+@NgModule({
+    declarations: [
+        HeaderComponent,
+    ]
+})
+export class LayoutModule {
+
+}
