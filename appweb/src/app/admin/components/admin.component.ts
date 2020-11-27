2d61a2e6f868826b4a88cd765fa8a958920413b7
Seba <sebastianbellidodg@gmail.com>
2020-11-27 12:08:38 -0300

    download .csv and the dates are displayed correctly

--- appweb/src/app/admin/components/admin.component.ts
+++ appweb/src/app/admin/components/admin.component.ts
@@ -21,26 +21,25 @@ export class AdminComponent implements OnInit {
   public creativesUsers = [];
   public count = 1;
   public end = false;
-  
+  // public admin: AdminComponent
+
   constructor(
     private http: HttpClient,
     private router: Router,
     private dbData: DataDbService,
     private FileSaverService: FileSaverService,
     private afStorage: AngularFirestore,
+
     
   ) { }
 
 
-
-  // al iniciar esta sección, se obtienen 
-  //todos los datos de la colección 'creativesUsers'
+//al iniciar esta sección, se obtienen todos los datos de la colección 'creativesUsers'
   ngOnInit(): void {
     this.dbData.getAllUser().subscribe((usersSnapshop) => {
       usersSnapshop.forEach((usersData: any) => {
         this.creativesUsers.push({
           id: usersData.payload.doc.id,
-          // date: usersData.payload.doc.dateStart,
           data: usersData.payload.doc.data()         
         });
       });
@@ -48,11 +47,55 @@ export class AdminComponent implements OnInit {
   }
 
   public getData() {
-    let data = this.dbData.getData();
+    let data = this.dbData.getCreativesUsersData(this);
     return data;
   }
 
-  
+  downloadFile(data: any) {
+    let csvData = this.ConvertToCSV(data, ['nameLastName','age', 'city', 'aducationLevel', 'educationStatus', 
+    'school', 'degree', 'year', 'grade', 'course', 'object', 'proposal', 'dateStart', 'dateEnd']);
+     
+      let blob = new Blob(['\ufeff' + csvData], { type: 'text/csv;charset=utf-8;' });
+      let dwldLink = document.createElement("a");
+      let url = URL.createObjectURL(blob);
+      let isSafariBrowser = navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1;
+      
+      if (isSafariBrowser) {  //if Safari open in new window to save file with random filename.
+          dwldLink.setAttribute("target", "_blank");
+      }
+    
+      dwldLink.setAttribute("href", url);
+      dwldLink.setAttribute("download", "dataUsersCreatives" + ".csv");
+      dwldLink.style.visibility = "hidden";
+      document.body.appendChild(dwldLink);
+      dwldLink.click();
+      document.body.removeChild(dwldLink);
+    }
+    
+    ConvertToCSV(objArray : any, headerList : any) {
+       let array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
+       let str = '';
+       let row = 'S.No,';
+    
+        for (let index in headerList) {
+           row += headerList[index] + ',';
+       }
+    
+       row = row.slice(0, -1);
+       str += row + '\r\n';
+    
+       for (let i = 0; i < array.length; i++) {
+           let line = (i+1)+'';
+           for (let index in headerList) {
+              let head = headerList[index];
+               line += ',' + array[i][head];
+              //  console.log(array[i]);
+               
+           }    
+           str += line + '\r\n';
+       }
+       return str;
+    }
  
 
   
--- appweb/src/app/core/services/db/data-db.service.ts
+++ appweb/src/app/core/services/db/data-db.service.ts
@@ -3,6 +3,7 @@ import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreModule, D
 
 import { CreativeUser } from './../../models/creative-user.interface';
 import { HttpClient, HttpResponse } from '@angular/common/http';
+import { AdminComponent } from 'src/app/admin/components/admin.component';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +11,7 @@ import { HttpClient, HttpResponse } from '@angular/common/http';
 
 export class DataDbService {
   private creativesCollection: AngularFirestoreCollection<CreativeUser>;
+  public creativesUsers = [];
 
   constructor(private afs: AngularFirestore, private http: HttpClient) {  
     this.creativesCollection = afs.collection<any>('creatives-users');
@@ -21,76 +23,22 @@ export class DataDbService {
   }
 
   public getAllUser() {
-    console.log(this.afs.collection('creatives-users').snapshotChanges());   
     return this.afs.collection('creatives-users').snapshotChanges();
   }
 
-  public getData() {
+  public getCreativesUsersData(admin: AdminComponent) {
   // Asigna la instantánea para incluir el ID del documento
-  this.creativesCollection.snapshotChanges().subscribe((usersSnapshop) => { 
-    return usersSnapshop.map(user => { 
-      const data = user.payload.doc.data()
-      const id = user.payload.doc.id
-
-      this.downloadFile(data);
-      // this.downloadFile(data, 'json')
-      
-      // return { data, id }
-      return data;
-    })
-  })
+  this.afs.collection('creatives-users').snapshotChanges().subscribe((usersSnapshop) => {
+    usersSnapshop.forEach((usersData: any) => {
+      this.creativesUsers.push(
+        usersData.payload.doc.data()       
+      );
+    });
+    admin.downloadFile(this.creativesUsers);
+  });
 }  
 
 
-downloadFile(data : any) {
-  const json = JSON.stringify(data);
-  console.log(json);
-  
-//   let csvData = this.ConvertToCSV(json, ['nameLastName','age', 'city', 'aducationLevel', 'educationStatus', 
-// 'school', 'degree', 'year', 'grade', 'course', 'object', 'proposal', 'dateStart', 'dateEnd']);
 
-let csvData = this.ConvertToCSV(json, data);
-
- 
-  let blob = new Blob(['\ufeff' + csvData], { type: 'text/csv;charset=utf-8;' });
-  let dwldLink = document.createElement("a");
-  let url = URL.createObjectURL(blob);
-  let isSafariBrowser = navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1;
-  
-  if (isSafariBrowser) {  //if Safari open in new window to save file with random filename.
-      dwldLink.setAttribute("target", "_blank");
-  }
-
-  dwldLink.setAttribute("href", url);
-  dwldLink.setAttribute("download", "filename" + ".csv");
-  dwldLink.style.visibility = "hidden";
-  document.body.appendChild(dwldLink);
-  dwldLink.click();
-  document.body.removeChild(dwldLink);
-  console.log(csvData);
-}
-
-ConvertToCSV(objArray : any, headerList : any) {
-   let array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
-   let str = '';
-   let row = 'S.No,';
-
-    for (let index in headerList) {
-       row += headerList[index] + ',';
-   }
-
-   row = row.slice(0, -1);
-   str += row + '\r\n';
-
-   for (let i = 0; i < array.length; i++) {
-       let line = (i+1)+'';
-       for (let index in headerList) {
-          let head = headerList[index];
-           line += ',' + array[i][head];
-       }    
-       str += line + '\r\n';
-   }
-   return str;
-}
 }
 
