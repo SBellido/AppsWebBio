8f859d59d71b76edb9c268fdd3cf1e696b729ee9
Juan Ignacio <jimartinezabadias@gmail.com>
2021-01-21 17:16:36 -0300

    Admin/Add progress spinner (#7)

--- appweb/src/environments/environment.prod.ts
+++ appweb/src/environments/environment.prod.ts
@@ -1,3 +1,17 @@
+// export const environment = {
+//   production: true,
+//   firebaseConfig : {
+//     apiKey: 'AIzaSyAzvJ5tF7OHEBGcTv9oB0B3Q2hlG8o2cVQ',
+//     authDomain: 'investigapp-ec690.firebaseapp.com',
+//     databaseURL: 'https://investigapp-ec690.firebaseio.com',
+//     projectId: 'investigapp-ec690',
+//     storageBucket: 'investigapp-ec690.appspot.com',
+//     messagingSenderId: '928587026895',
+//     appId: '1:928587026895:web:e134d071fd370d9fd7c2ba',
+//     measurementId: 'G-214XGZYCLF'
+//   }
+// };
+
 export const environment = {
   production: true,
   firebaseConfig : {
