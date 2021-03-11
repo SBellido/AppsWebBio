576cb9d3376c85a9659ecfef991a3a1c0ff6d6ba
Seba <sebastianbellidodg@gmail.com>
2021-03-11 20:28:17 -0300

    carousel instructionRulit ok

--- appweb/src/app/rulit/components/rulit-instructions/rulit-instructions.component.ts
+++ appweb/src/app/rulit/components/rulit-instructions/rulit-instructions.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+import Swiper from 'swiper';
 
 @Component({
     selector: 'app-rulit-instructions',
@@ -9,11 +10,31 @@ import { Component, OnInit } from '@angular/core';
 
 export class RulitInstructionsComponent implements OnInit {
 
+    mySwiper: Swiper;
+
     constructor() {}
 
     ngOnInit(): void {
         
     }
+
+    ngAfterViewInit() {
+        this.mySwiper = new Swiper('.swiper-container', {
+          // If we need pagination
+          pagination: {
+            el: '.swiper-pagination',
+          },
+          // Navigation arrows
+          navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+          },
+          // And if we need scrollbar
+          scrollbar: {
+            el: '.swiper-scrollbar',
+          },
+        });
+      }
 
 }
 
