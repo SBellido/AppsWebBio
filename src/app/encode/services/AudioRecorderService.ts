fba44170e690a11b488a176f2ff7049139b20499
Juan Ignacio <jimartinezabadias@gmail.com>
2021-06-14 20:15:25 -0300

    add types for MediaRecorder

--- src/app/encode/services/AudioRecorderService.ts
+++ src/app/encode/services/AudioRecorderService.ts
@@ -9,13 +9,36 @@ export class AudioRecorderService implements IAudioRecorder {
     isRecording: boolean = false;
     audioList: any[];
 
+    private _options = {mimeType: 'audio/webm'};
+
     constructor()
     {
     }    
     
-    record(): void {
+    record(stream: MediaStream): void {
         this.isRecording = true;
         console.log("recording...");
+
+        const recordedChunks = [];
+        const mediaRecorder = new MediaRecorder(stream, this._options);
+
+        // mediaRecorder.addEventListener('dataavailable', function(e) {
+        // if (e.data.size > 0) {
+        //     recordedChunks.push(e.data);
+        // }
+
+        // if(shouldStop === true && stopped === false) {
+        //     mediaRecorder.stop();
+        //     stopped = true;
+        // }
+        // });
+
+        // mediaRecorder.addEventListener('stop', function() {
+        // downloadLink.href = URL.createObjectURL(new Blob(recordedChunks));
+        // downloadLink.download = 'acetest.wav';
+        // });
+
+        // mediaRecorder.start();
     }
     
     stopRecording(): void {
--- src/app/encode/services/IAudioRecorderService.ts
+++ src/app/encode/services/IAudioRecorderService.ts
@@ -1,6 +1,6 @@
 export interface IAudioRecorder {
     isRecording: boolean;
     audioList: Array<any>;
-    record(): void;
+    record(stream: MediaStream): void;
     stopRecording(): void;
 }
