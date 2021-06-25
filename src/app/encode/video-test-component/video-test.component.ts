5c984426d45ea54813dfe30c305f556eb7d6aff1
Juan Ignacio <jimartinezabadias@gmail.com>
2021-06-24 22:16:00 -0300

    navigate to video test

---
+++ src/app/encode/video-test-component/video-test.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-encode-video-test',
+    templateUrl: './video-test.component.html',
+    styleUrls: ['../encode.component.scss']
+})
+export class EncodeVideoTestComponent implements OnInit {
+
+  constructor() 
+  {
+  }
+
+  ngOnInit(): void 
+  {
+  }
+
+}
