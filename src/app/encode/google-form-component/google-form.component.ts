5d2dbf11c9e5a3b03121221415fa84b660ab9382
Juan Ignacio <jimartinezabadias@gmail.com>
2021-06-29 00:12:53 -0300

    init google form component

---
+++ src/app/encode/google-form-component/google-form.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-google-form',
+    templateUrl: './google-form.component.html',
+    styleUrls: ['../encode.component.scss']
+})
+
+export class GoogleFormComponent {
+  
+  constructor() 
+  {
+  }
+  
+}
