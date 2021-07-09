34063592d98c3421fb875331104346c3cc021897
Juan Ignacio <jimartinezabadias@gmail.com>
2021-07-09 12:34:57 -0300

    encode: add fields to invite form

--- src/app/admin/components/admin-encode/invite-form-component/invite-form.component.ts
+++ src/app/admin/components/admin-encode/invite-form-component/invite-form.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-invite-form',
   templateUrl: './invite-form.component.html',
-  styleUrls: ['../../admin.component.scss'],
+  styleUrls: ['invite.scss','../../admin.component.scss'],
 })
 export class InviteFormComponent {
+
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
+  
+  nameFormControl = new FormControl('', [
+    Validators.required
+  ]);
 
   constructor(public dialogRef: MatDialogRef<InviteFormComponent>) {}
 
