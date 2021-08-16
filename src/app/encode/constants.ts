f5595dc2574612711c220367bc7d16f0ef43951d
Juan Ignacio <jimartinezabadias@gmail.com>
2021-08-16 10:49:08 -0300

    forms styles

--- src/app/encode/constants.ts
+++ src/app/encode/constants.ts
@@ -25,12 +25,12 @@ export enum EducationLevels
 
 export enum SomnolenceDegrees
 {
-    totallyAwake = "Me siento activo, vital, alerta o bien despierto",
-    veryHigh = "Funcionando a niveles altos, pero no completamente alerta",
-    relaxed= "Despierto, pero relajado; sensible pero no completamente alerta",
-    littleConfused= "Un poco confundido, decepcionado",
-    confused= "Confundido; pierdo interés en permanecer despierto; ralentizado",
-    tired= "Somnoliento, mareado, luchado contra el sueño; prefiero recostarme",
-    almostSlept= "Ya no lucho contra el sueño, comenzaré a dormirme pronto; tengo pensamientos como sueños",
-    slept= "Dormido"
+    totallyAwake = "Me siento activo, vital, alerta o bien despierto.",
+    veryHigh = "Funcionando a niveles altos, pero no completamente alerta.",
+    relaxed= "Despierto, pero relajado; sensible pero no completamente alerta.",
+    littleConfused= "Un poco confundido, decepcionado.",
+    confused= "Confundido; pierdo interés en permanecer despierto; ralentizado.",
+    tired= "Somnoliento, mareado, luchado contra el sueño; prefiero recostarme.",
+    almostSlept= "Ya no lucho contra el sueño, comenzaré a dormirme pronto; tengo pensamientos como sueños.",
+    slept= "Dormido."
 }
