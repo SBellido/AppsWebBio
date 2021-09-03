2dd5d3ff9e70d915a76436d8dfadf3659e40fa0b
Juan Ignacio <jimartinezabadias@gmail.com>
2021-09-02 23:52:25 -0300

    add forms to forms list

--- src/app/encode/form-list-component/form-list.component.ts
+++ src/app/encode/form-list-component/form-list.component.ts
@@ -15,7 +15,21 @@ export class EncodeFormListComponent {
 
   public openForm(formName: string)
   {
-    let windowRef: Window = window.open("https://forms.gle/cQEKMhNgn2fr2MQdA","formPopup","width=600,height=600");
+    let windowRef: Window;
+    switch (formName) {
+      case 'stai':
+        windowRef = window.open("https://forms.gle/aXHRPZz4v2fohMUj6","formPopup","width=600,height=600");
+        break;
+      case 'panas':
+        windowRef = window.open("https://forms.gle/fgbU4Zx9gxwcWB8V6","formPopup","width=600,height=600");
+        break;
+      case 'ipip':
+        windowRef = window.open("https://forms.gle/YiQw42ZyxHx6WK1Z9","formPopup","width=600,height=600");
+        break;
+    
+      default:
+        break;
+    }
   }
   
 }
