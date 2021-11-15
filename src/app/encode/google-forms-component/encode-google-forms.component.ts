4358ce1a9c0940aca46fc53795bef03a7fa5ede1
Juan Ignacio <jimartinezabadias@gmail.com>
2021-11-14 23:21:15 -0300

    listen to changes in google forms

--- src/app/encode/google-forms-component/encode-google-forms.component.ts
+++ src/app/encode/google-forms-component/encode-google-forms.component.ts
@@ -1,37 +1,44 @@
 import { Component } from '@angular/core';
-import { DomSanitizer } from '@angular/platform-browser';
+import { Observable } from 'rxjs';
 import { IEncodeGoogleFormResponse } from '../models/IEncodeGoogleFormResponse';
+import { IEncodeUser } from '../models/IEncodeUser';
 import { EncodeUserService } from '../services/EncodeUserService';
-import {STEPPER_GLOBAL_OPTIONS} from '@angular/cdk/stepper';
 
 @Component({
     selector: 'app-encode-google-forms',
     templateUrl: './encode-google-forms.component.html',
-    styleUrls: ['encode-google-forms.component.scss','../encode.component.scss'],
-    providers: [
-      {
-        provide: STEPPER_GLOBAL_OPTIONS,
-        useValue: {showError: true},
-      },
-    ]
+    styleUrls: ['encode-google-forms.component.scss','../encode.component.scss']
 })
 
-export class EncodeGoogleFormsComponent{
+export class EncodeGoogleFormsComponent {
   
   public userForms: IEncodeGoogleFormResponse[] = null;
   
   public embeddedParameter: string = "embedded=true"; 
 
-  constructor(private _userService: EncodeUserService, private _sanitizer: DomSanitizer) 
+  constructor(private _userService: EncodeUserService) 
   {
-    this.userForms = this._userService.user().googleFormsResponses;
-    // console.log(this.userForms);
+    // this.userForms = this._userService.user().googleFormsResponses;
+    this._userService.loadUser$(this._userService.user().uid);
+    this._userService.user$().subscribe(this._userObserver);
+  }
+
+  public openForm(formURL: string): void
+  {
+    let windowRef: Window;
+    windowRef = window.open(formURL,"formPopup","width=600,height=600");
+  }
+
+  public isFormDisabled(formIndex: number): boolean
+  {
+    const isEnabled = this.userForms[formIndex - 1] ? this.userForms[formIndex - 1].isResponded : true;  
+    return !isEnabled;
+  }
+
+  private _userObserver = (user: IEncodeUser) => {
+    console.log("user updated");
+    console.log(user);
+    this.userForms = user.googleFormsResponses;
   }
   
 }
-
-// https://desa.local:4200/encode/BzgDblC111Wc7HCc4lx6
-
-
-//             "https://docs.google.com/forms/d/e/1FAIpQLSf3hQ7SIgqmEH1cu126f5Yt9hNoaE472hhgI_cBAaufVQ7t_A/viewform?usp=pp_url&entry.103590893=dsg@gasdg.com"
-// <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdQwFHeG7ZqL5M2ODDnMMHIViSYTYMddx1tYTHjmXEnYXy7lg/viewform?embedded=true" width="640" height="740" frameborder="0" marginheight="0" marginwidth="0">Cargando…</iframe>
