0d2ce67ed2c2dc7db80f01c8c8824845ff4e2f66
Juan Ignacio <jimartinezabadias@gmail.com>
2021-12-18 23:35:57 -0300

    form control validator

--- src/app/encode/google-forms-component/encode-google-forms.component.ts
+++ src/app/encode/google-forms-component/encode-google-forms.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { IEncodeGoogleFormResponse } from '../models/IEncodeGoogleFormResponse';
 import { EncodeUserService } from '../services/EncodeUserService';
-import { FormArray, FormBuilder, FormControl, FormGroup, ValidationErrors, Validators } from "@angular/forms";
+import { FormArray, FormBuilder, FormControl, FormGroup, ValidationErrors } from "@angular/forms";
 import { Observable, of } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { catchError, switchMap, tap } from 'rxjs/operators';
 
 
 @Component({
@@ -17,8 +17,6 @@ export class EncodeGoogleFormsComponent implements OnInit {
   private _userResponses: Array<IEncodeGoogleFormResponse> = null;
   
   public userForms: FormGroup;
-  
-  // public embeddedParameter: string = "embedded=true"; 
 
   constructor(private _userService: EncodeUserService, private _formBuilder: FormBuilder) 
   {
@@ -34,10 +32,9 @@ export class EncodeGoogleFormsComponent implements OnInit {
       googleForms: this._formBuilder.array([])
     });
 
-    console.log(this.googleForms);
-
     this._userResponses.forEach(preFilledResp => {
-      this.googleForms.push(this._formBuilder.control({ preFilledURL: preFilledResp.preFilledURL }, null, CustomValidator.googleFormResponse(this._userService)));
+      let newControl = this._formBuilder.control({ preFilledURL: preFilledResp.preFilledURL }, null, CustomValidator.googleFormResponse(this._userService));
+      this.googleForms.push(newControl);
     });
 
   }
@@ -46,46 +43,39 @@ export class EncodeGoogleFormsComponent implements OnInit {
     console.log('bla');
   }
 
-  // public openForm(formURL: string): void
-  // {
-  //   let windowRef: Window;
-  //   windowRef = window.open(formURL,"formPopup","width=600,height=600");
-  // }
-
-  // public isFormDisabled(formIndex: number): boolean
-  // {
-  //   const isEnabled = this.userForms[formIndex - 1] ? this.userForms[formIndex - 1].isResponded : true;  
-  //   return !isEnabled;
-  // }
-
-  // private _userObserver = (user: IEncodeUser) => {
-  //   this.userForms = user.googleFormsResponses;
-  // }
-  
 }
 
 export class CustomValidator {
+  
   static googleFormResponse(userService: EncodeUserService){
+    
     return (control: FormControl): Observable<ValidationErrors | null>  => {
 
-      const responseURL: string = control.value.preFilledURL;
+      const actualResponseURL: string = control.value.preFilledURL;
 
       return userService.googleForms$.pipe(
-        map(arr => arr.filter(resp => {
-          // console.log('actual form url: ');
-          // console.log(responseURL);
-          // console.log('actual resp to compare: ');
-          // console.log(resp.preFilledURL);
-          // console.log('is responded: ');
-          // console.log(resp.isResponded);
-          if ((resp.preFilledURL == responseURL) && resp.isResponded) {
-            console.log('form has been responded');
+        switchMap(arr => { 
+          arr = arr.filter(resp => (resp.preFilledURL == actualResponseURL) && resp.isResponded );
+
+          if (arr.length > 0){
             return of(null);
-          } 
-         
-          return of({ 'error': 'bla'});
-        }))
-      );
-    }
+          }
+
+          return of({'error': 'error'});
+        }), 
+        catchError(() => of({'error': 'error'}))
+        );
+      }
+    }    
   }
+  
+
+function next(next: any, arg1: () => void): any {
+  throw new Error('Function not implemented.');
 }
+  // console.log('actual form url: ');
+  // console.log(responseURL);
+  // console.log('actual resp to compare: ');
+  // console.log(resp.preFilledURL);
+  // console.log('is responded: ');
+  // console.log(resp.isResponded);
