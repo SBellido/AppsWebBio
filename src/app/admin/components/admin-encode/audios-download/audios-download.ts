802afc32fedaa21b829d67cc8ec5dc22085e118e
Juan Ignacio <jimartinezabadias@gmail.com>
2022-01-07 14:21:21 -0300

    initialize dayOne in null

--- src/app/admin/components/admin-encode/audios-download/audios-download.ts
+++ src/app/admin/components/admin-encode/audios-download/audios-download.ts
@@ -10,27 +10,45 @@ import * as JSZip from 'jszip';
     selector: 'app-admin-audios-download',
     templateUrl: './audios-download.component.html',
     styleUrls: ['./audios-download.component.scss','../../admin.component.scss'],
-  })
-  export class AudiosDownload implements OnInit{
+})
+export class AudiosDownload implements OnInit{
 
-    constructor(private httpClient: HttpClient,
-                private route: ActivatedRoute,
+    constructor(private _httpClient: HttpClient,
+                private _route: ActivatedRoute,
                 private _dbService: DataDbService) {
     }
-    
+
     async ngOnInit(): Promise<void> 
     {
-        let userIdParam = this.route.snapshot.paramMap.get('userId');
-        this.downloadZippedFiles((await this._dbService.getEncodeUser(userIdParam)).dayOne.audios, "audios_"+userIdParam);
+        let userIdParam = this._route.snapshot.paramMap.get('userId');
+        const userData = await this._dbService.getEncodeUser(userIdParam);
+        let userAudios = new Array<IEncodeAudio>();
+
+        if (userData.dayOne != null) {
+            userData.dayOne.audios.map((audio: IEncodeAudio) => {
+                userAudios.push(audio);
+            })
+        }
+
+        // TODO: agregar cuando dayTwo tenga audios
+        // if (userData.dayTwo != null) {
+        //     userData.dayTwo.audios.map((audio: IEncodeAudio) => {
+        //         userAudios.push(audio);
+        //     })
+        // }
+
+        const zipFileName = "audios_" + userIdParam;
+        
+        this.downloadZippedFiles(userAudios, zipFileName);
     }
-    
+
     public downloadZippedFiles(files: Array<IEncodeAudio>, zipName: string): void {
-    
+
         const zipFile: JSZip = new JSZip();
         let count = 0;
-    
+
         files.forEach(file => {
-            this.httpClient.get(file.downloadURL, {responseType: 'blob'}).subscribe(response => {
+            this._httpClient.get(file.downloadURL, {responseType: 'blob'}).subscribe(response => {
             zipFile.file(file.id, response, {binary: true});
             count++;
             if (count === files.length) {
@@ -42,4 +60,4 @@ import * as JSZip from 'jszip';
         
         });
     }
-    }
+}
