5e619237d2d52bf5fd5218115069e0b52a45e527
Juan Ignacio <jimartinezabadias@gmail.com>
2022-01-29 21:16:59 -0300

    get suspect lineups from db

--- src/app/encode/models/IEncodeSessionTwo.ts
+++ src/app/encode/models/IEncodeSessionTwo.ts
@@ -1,11 +1,11 @@
-import { PerpetratorCondition, PerpetratorId, SomnolenceDegree } from "../constants";
+import { PerpetratorCondition, Perpetrator1Id, Perpetrator2Id,  SomnolenceDegree } from "../constants";
 import { IEncodeIdentificationResponse } from "./IEncodeIdentificationResponse";
 import { IEncodeImageSelectionResponse } from "./IEncodeImageSelectionResponse";
 
 export interface IEncodeSessionTwo {
     somnolenceDegree: SomnolenceDegree;
     perpetratorCondition: PerpetratorCondition | null;
-    identificationResponse: Map<PerpetratorId, IEncodeIdentificationResponse> | null;
     imageSelectionResponse: IEncodeImageSelectionResponse[] | null;
+    identificationResponse: Map< typeof Perpetrator1Id | typeof Perpetrator2Id , IEncodeIdentificationResponse> | null;
     completed: boolean | null;
 }
--- src/app/encode/suspect-identification-component/suspect-identification.component.ts
+++ src/app/encode/suspect-identification-component/suspect-identification.component.ts
@@ -5,6 +5,9 @@ import { ActivatedRoute, Router, UrlTree } from '@angular/router';
 import { OnExit } from '../exit.guard';
 import { Observable } from 'rxjs';
 import { DataDbService } from 'src/app/core/services/db/data-db.service';
+import { PerpetratorCondition } from '../constants';
+import { IEncodeSuspect } from '../models/IEncodeSuspect';
+import { DocumentReference } from '@angular/fire/firestore';
 
 @Component({
     selector: 'app-suspect-identification',
@@ -27,8 +30,39 @@ export class EncodeSuspectIdentificationComponent implements OnExit {
   }
 
   async openIdentificaton() {
-    const suspects = await this._dbService.getEncodeSuspects();
     const perpetratorCondition = this._userService.user.sessionTwo.perpetratorCondition;
+    const taskResources = await this._dbService.getEncodeTasksResources();
+    const perp1suspects = this._getPerpetratorSuspects(taskResources.perpetrator1Suspects); 
+    const perp2suspects = this._getPerpetratorSuspects(taskResources.perpetrator2Suspects); 
+    let firstLineup: Array<IEncodeSuspect>;
+    let secondLineup: Array<IEncodeSuspect>;
+    
+    if(perpetratorCondition == PerpetratorCondition.A){
+      firstLineup = perp1suspects;
+      // quitar uno de los de relleno
+      secondLineup = perp2suspects.filter( suspect => suspect.isPerpetrator == false );
+    }
+    
+    if(perpetratorCondition == PerpetratorCondition.B){
+      firstLineup = perp2suspects;
+      // quitar uno de los de relleno
+      secondLineup = perp1suspects.filter( suspect => suspect.isPerpetrator == false );
+    }
+    
+    console.log(firstLineup);
+    console.log(secondLineup);
   }
-  
+
+  private _getPerpetratorSuspects(suspectDocuments: DocumentReference<IEncodeSuspect>[]): Array<IEncodeSuspect> {
+    let suspects = new Array<IEncodeSuspect>();
+   
+    suspectDocuments.forEach( async docRef => {
+      const suspectId = docRef.id;
+      const suspect = await this._dbService.getEncodeSuspect(suspectId);
+      suspects.push(suspect);
+    });
+
+    return suspects;
+  }
+
 }
