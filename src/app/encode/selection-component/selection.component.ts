047368744ff31b2683ae1708cb0dde39a8b4e945
nahurup <nahuelruppel10@gmail.com>
2022-01-31 15:37:02 -0300

    template selection-component

--- src/app/encode/selection-component/selection.component.ts
+++ src/app/encode/selection-component/selection.component.ts
@@ -1,26 +1,34 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, ViewChild, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
     selector: 'app-encode-selection',
     templateUrl: './selection.component.html',
     styleUrls: ['selection.component.scss','../encode.component.scss']
 })
-export class EncodeSelectionComponent {
+export class EncodeSelectionComponent implements OnInit {
+  firstFormGroup: FormGroup;
+  secondFormGroup: FormGroup;
 
   constructor(private _router: Router,
-              private _route: ActivatedRoute) 
+              private _route: ActivatedRoute,
+              private _formBuilder: FormBuilder) 
   {
   }
 
-  ngOnInit(): void 
+  ngOnInit() 
   {
-
+    this.firstFormGroup = this._formBuilder.group({
+      firstCtrl: ['', Validators.required],
+    });
+    this.secondFormGroup = this._formBuilder.group({
+      secondCtrl: '',
+    });
   }
-
   onConfirm(): any 
   {
-    this._router.navigate(["../consent"], { relativeTo: this._route });
+    //this._router.navigate(["../consent"], { relativeTo: this._route });
   }
 
 }
