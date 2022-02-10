b3b24e2ebcd3fafa1686fd21e6844e75ecd051e4
Juan Ignacio <jimartinezabadias@gmail.com>
2022-02-09 22:03:07 -0300

    add id to suspect

--- src/app/encode/models/IEncodeSuspect.ts
+++ src/app/encode/models/IEncodeSuspect.ts
@@ -1,6 +1,7 @@
 import { Perpetrator1Id, Perpetrator2Id } from "../constants";
 
 export interface IEncodeSuspect {
+    id: string|null;
     photoStorageRef: string;
     isPerpetrator: boolean;
     suspectOfBeing: typeof Perpetrator1Id | typeof Perpetrator2Id
