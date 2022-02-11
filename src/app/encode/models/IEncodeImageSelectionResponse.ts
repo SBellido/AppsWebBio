f9754569fa353bf7829c3be1383abb0278fe9ac6
nahurup <nahuelruppel10@gmail.com>
2022-02-11 18:55:28 -0300

    interface name changed

--- src/app/encode/models/IEncodeImageSelectionResponse.ts
+++ src/app/encode/models/IEncodeImageSelectionResponse.ts
@@ -1,4 +1,4 @@
-export interface IEncodeImageSelection {
+export interface IEncodeImageSelectionResponse {
     pairNumber: number | null;
     isReal : boolean | null;
     imageURL: string | null;
