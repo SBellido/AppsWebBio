8b8949e74f30f24ef6649d31a2503b1dfbdcad16
Juan Ignacio <jimartinezabadias@gmail.com>
2022-02-14 19:40:17 -0300

    add photoUrl to suspect interface

--- src/app/encode/models/IEncodeSuspect.ts
+++ src/app/encode/models/IEncodeSuspect.ts
@@ -2,6 +2,7 @@ import { PERPETRATOR_1_ID, PERPETRATOR_2_ID } from "../constants";
 
 export interface IEncodeSuspect {
     id: string|null;
+    photoImageUrl: string|null;
     photoStorageRef: string;
     isPerpetrator: boolean;
     suspectOfBeing: typeof PERPETRATOR_1_ID|typeof PERPETRATOR_2_ID|null;
