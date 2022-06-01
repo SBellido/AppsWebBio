c9e4059e621bc2b03aa1917c5015971f2e93cc09
Juan Ignacio <jimartinezabadias@gmail.com>
2022-06-01 19:57:47 -0300

    create encode invite using modular api

--- src/app/admin/components/admin-encode/admin-encode.component.ts
+++ src/app/admin/components/admin-encode/admin-encode.component.ts
@@ -2,13 +2,14 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MatPaginator } from '@angular/material/paginator';
 import { tap } from 'rxjs/operators';
-import { DataDbService } from 'src/app/core/services/db/data-db.service';
+// import { DataDbService } from 'src/app/core/services/db/data-db.service';
 import { EncodeUserService } from 'src/app/encode/services/EncodeUserService';
 import { EncodeUsersDataSource } from './encodeUsersDataSource';
 import { InviteFormComponent } from './invite-form-component/invite-form.component';
 import { Router } from '@angular/router';
 import { Parser, transforms } from 'json2csv';
 import { PlatformLocation } from '@angular/common';
+import { encodeCSVFields } from '../../constants';
 
 
 const SEPARATOR = "_";
@@ -22,58 +23,7 @@ export class AdminEncodeComponent implements OnInit {
 
   @ViewChild(MatPaginator) private _paginator: MatPaginator;
   private _pageIndex: number = 0;
-  private _csvFields = [
-    "uid",
-    "name",
-    "email",
-    "creationDate",
-    "abandonedByUser",
-    "consent_hasAccepted",
-    "consent_date",
-    "personalInfo_age",
-    "personalInfo_educationLevel",
-    "personalInfo_gender",
-    "personalInfo_occupation",
-    "personalInfo_ongoingCareer",
-    "healthInfo_cronicMedicines",
-    "healthInfo_hasSleepDisorder",
-    "healthInfo_sleepDisorders",
-    "healthInfo_takesCronicMedicine",
-    "sessionOne_completed",
-    "sessionOne_somnolenceDegree",
-    "sessionTwo_completed",
-    "sessionTwo_somnolenceDegree",
-    "sessionTwo_perpetratorCondition",
-    "googleFormsResponses_0_isResponded",
-    "googleFormsResponses_1_isResponded",
-    "googleFormsResponses_2_isResponded",
-    "googleFormsResponses_3_isResponded",
-    "googleFormsResponses_4_isResponded",
-    "googleFormsResponses_5_isResponded",
-    "sessionTwo_identificationResponse_0_confidenceLevel",
-    "sessionTwo_identificationResponse_0_selectedSuspect_id",
-    "sessionTwo_identificationResponse_0_selectedSuspect_isPerpetrator",
-    "sessionTwo_identificationResponse_1_confidenceLevel",
-    "sessionTwo_identificationResponse_1_selectedSuspect_id",
-    "sessionTwo_identificationResponse_1_selectedSuspect_isPerpetrator",
-    "sessionTwo_imageSelectionResponse_0_id",
-    "sessionTwo_imageSelectionResponse_1_id",
-    "sessionTwo_imageSelectionResponse_2_id",
-    "sessionTwo_imageSelectionResponse_3_id",
-    "sessionTwo_imageSelectionResponse_4_id",
-    "sessionTwo_imageSelectionResponse_5_id",
-    "sessionTwo_imageSelectionResponse_6_id",
-    "sessionTwo_imageSelectionResponse_7_id",
-    "sessionTwo_imageSortingResponse_0_id",
-    "sessionTwo_imageSortingResponse_1_id",
-    "sessionTwo_imageSortingResponse_2_id",
-    "sessionTwo_imageSortingResponse_3_id",
-    "sessionTwo_imageSortingResponse_4_id",
-    "sessionTwo_imageSortingResponse_5_id",
-    "sessionTwo_imageSortingResponse_6_id",
-    "sessionTwo_imageSortingResponse_7_id",
-    "link_audios"
-  ];
+  private _csvFields = encodeCSVFields;
 
   public usersDataSource: EncodeUsersDataSource;
   public totalTestsCounter: any = { count: -1 };
@@ -86,17 +36,16 @@ export class AdminEncodeComponent implements OnInit {
   constructor(
     private _encodeUserService: EncodeUserService,
     private _dialog: MatDialog,
-    private _dbService: DataDbService,
     private _router: Router,
     private _platformLocation: PlatformLocation) {}
   
   ngOnInit(): void 
   {
-    this.usersDataSource = new EncodeUsersDataSource(this._dbService);
-    this.usersDataSource.loadUsers(this.pageSize);
+    // this.usersDataSource = new EncodeUsersDataSource(this._dbService);
+    // this.usersDataSource.loadUsers(this.pageSize);
 
-    // Get total number of users
-    this._dbService.getEncodeMetadataCounter().snapshotChanges().subscribe( this._userCounterObserver );
+    // // Get total number of users
+    // this._dbService.getEncodeMetadataCounter().snapshotChanges().subscribe( this._userCounterObserver );
   }
 
   ngAfterViewInit() {
@@ -121,27 +70,11 @@ export class AdminEncodeComponent implements OnInit {
     this._pageIndex = this._paginator.pageIndex;
   }
   
+  // INVITE
   public openInviteDialog(): void {
     const dialogRef = this._dialog.open(InviteFormComponent);
 
     dialogRef.afterClosed().subscribe(this._inviteDialogClosed$);
-  }
-
-  public getUserResults(uid) {
-    this._router.navigate(['/admin/encode/', uid]);
-  }
-
-  public async downloadCSV() {
-    let encodeUsers = await this._dbService.getAllEncodeUsersData();
-    let temp = JSON.parse(JSON.stringify(encodeUsers));
-    let downloadPrefix = (this._platformLocation as any).location.origin + "/admin/encode/";
-    for (let i = 0; i < temp.length; i++) {
-      temp[i].link_audios = downloadPrefix + temp[i].uid + "/audios";
-      temp[i].creationDate = new Date(temp[i].creationDate.seconds*1000);
-    }
-
-    let csvString = this.generateCSV(temp);
-    this.openDownloadWindow(csvString);
   }
 
   private _inviteDialogClosed$ = async (userData: { name: string, email: string }) => {
@@ -153,6 +86,25 @@ export class AdminEncodeComponent implements OnInit {
       this.isLoading = false;
     }
   }
+
+  public getUserResults(uid) {
+    this._router.navigate(['/admin/encode/', uid]);
+  }
+
+  public async downloadCSV() {
+    // let encodeUsers = await this._dbService.getAllEncodeUsersData();
+    // let temp = JSON.parse(JSON.stringify(encodeUsers));
+    // let downloadPrefix = (this._platformLocation as any).location.origin + "/admin/encode/";
+    // for (let i = 0; i < temp.length; i++) {
+    //   temp[i].link_audios = downloadPrefix + temp[i].uid + "/audios";
+    //   temp[i].creationDate = new Date(temp[i].creationDate.seconds*1000);
+    // }
+
+    // let csvString = this.generateCSV(temp);
+    // this.openDownloadWindow(csvString);
+  }
+
+  
 
   private _userCounterObserver = (counterData) => {
     this.totalTestsCounter = counterData.payload.data();
