56dfcd8d7f3dde8716261f3b6e94b567a83af9d1
Juan Ignacio <jimartinezabadias@gmail.com>
2022-06-01 19:57:47 -0300

    create encode invite using modular api

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DataDbService } from './services/db/data-db.service';
+import { FirestoreService } from './firestore.service';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule
   ],
-  providers: [ DataDbService ]
+  providers: [ DataDbService, FirestoreService ]
 })
 export class CoreModule { }
---
+++ src/app/core/firestore.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+
+import { collection, doc, Firestore, CollectionReference, DocumentReference, getDoc, setDoc } from '@angular/fire/firestore';
+import { IEncodeGoogleFormsSettings } from '../encode/models/IEncodeGoogleFormsSettings';
+import { IEncodeUser } from '../encode/models/IEncodeUser';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FirestoreService {
+  
+    private _encodeUserCollectionRef: CollectionReference<IEncodeUser>;
+    private _encodeConfigCollectionRef: CollectionReference;
+
+    constructor(
+    private _firestore: Firestore) { 
+
+    this._encodeUserCollectionRef = collection(this._firestore, "encode-users") as CollectionReference<IEncodeUser>;
+    this._encodeConfigCollectionRef = collection(this._firestore, "encode-config");
+
+    }
+
+    public getEncodeNewUserDocumentRef(): DocumentReference {
+        return doc(this._encodeUserCollectionRef);
+    }
+
+    async getEncodeGoogleFormsSettings(): Promise<IEncodeGoogleFormsSettings> {
+        const docRef = doc(this._firestore, this._encodeConfigCollectionRef.path, "googleFormsSettings");
+        const docSnap = await getDoc(docRef);
+
+        if (docSnap.exists()) {
+            return docSnap.data() as IEncodeGoogleFormsSettings;
+        } else {
+            // doc.data() will be undefined in this case
+            console.log("No such document!");
+        }
+
+        return null;
+    }
+
+    public createNewEncodeUser(user: IEncodeUser, userDocRef: DocumentReference): Promise<void> {
+        // const newUserRef = doc(this._encodeUserCollectionRef);
+        return setDoc(userDocRef, user);
+    }
+
+}
