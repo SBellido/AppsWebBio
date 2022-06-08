b94da61114e60d706fbb8d82185409248a14196e
Juan Ignacio <jimartinezabadias@gmail.com>
2022-06-08 18:58:51 -0300

    update Storage and audio list component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { DataDbService } from './services/db/data-db.service';
 import { EncodeFirestoreService } from './encodeFirestore.service';
+import { EncodeStorageService } from './encodeStorage.service';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule
   ],
-  providers: [ DataDbService, EncodeFirestoreService ]
+  providers: [ DataDbService, EncodeFirestoreService, EncodeStorageService ]
 })
 export class CoreModule { }
