1537c3808e568c7f570af94c987a973f3ab6a163
Juan Ignacio <jimartinezabadias@gmail.com>
2022-06-19 20:14:50 -0300

    update rulit result download

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,12 +4,13 @@ import { CommonModule } from '@angular/common';
 import { DataDbService } from './services/db/data-db.service';
 import { EncodeFirestoreService } from './encodeFirestore.service';
 import { EncodeStorageService } from './encodeStorage.service';
+import { RulitFirestoreService } from './rulitFirestore.service';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule
   ],
-  providers: [ DataDbService, EncodeFirestoreService, EncodeStorageService ]
+  providers: [ DataDbService, RulitFirestoreService, EncodeFirestoreService, EncodeStorageService ]
 })
 export class CoreModule { }
